1b47cb315eab14e318919bd0101ce77921946b56
anthpantaleo <anthpantaleo@gmail.com>
2022-10-12 21:10:00 -0400

    set lodash

--- src/index.js
+++ src/index.js
@@ -1,4 +1,20 @@
+import _ from "lodash";
 import "./styles.css";
 import Favicon from "./favicon.png";
 
+function component() {
+  const element = document.createElement("div");
+
+  // Lodash, currently included via a script, is required for this line to work
+  element.innerHTML = _.join(
+    ["Hello", "webpack", "it's Anthony Pantaleo"],
+    " "
+  );
+
+  return element;
+}
+
+document.body.appendChild(component());
+
 console.log("testing");
+console.log("sup");
